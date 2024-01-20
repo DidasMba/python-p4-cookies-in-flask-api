a895bd7907b77710e103a67b5fe563f4f234cbe4
DidasMba <daniel.mbarushi@gmail.com>
2024-01-20 18:52:15 +0300

    shosession

--- server/app.py
+++ server/app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, session, jsonify, make_response
+from flask import Flask, session, jsonify, make_response, request
+import os
 
 app = Flask(__name__)
-app.json.compact = False
 
-app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
+# Set a secret key for the session (needed for secure session handling)
+app.secret_key = os.urandom(24)
 
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
-
+    # Set values in the session object
     session["hello"] = session.get("hello") or "World"
     session["goodnight"] = session.get("goodnight") or "Moon"
 
+    # Create a response containing session and cookie information
     response = make_response(jsonify({
         'session': {
             'session_key': key,
@@ -18,13 +20,15 @@ def show_session(key):
             'session_accessed': session.accessed,
         },
         'cookies': [{cookie: request.cookies[cookie]}
-            for cookie in request.cookies],
+                     for cookie in request.cookies],
     }), 200)
 
+    # Set a new cookie
     response.set_cookie('mouse', 'Cookie')
 
     return response
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
+
     
